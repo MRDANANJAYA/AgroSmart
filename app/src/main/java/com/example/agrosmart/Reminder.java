b1bc56920d2cc3731feef2fca32f771519b5bf48
MRDANANJAYA <mrdana1234dana>
2022-03-08 15:23:35 +0530

    Updated march 08

--- app/src/main/java/com/example/agrosmart/Reminder.java
+++ app/src/main/java/com/example/agrosmart/Reminder.java
@@ -5,18 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.example.agrosmart.login.SignUp;
 import com.example.agrosmart.ml.ImageProcessing;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.ValueEventListener;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.EventListener;
+import java.util.Locale;
 
 public class Reminder extends AppCompatActivity {
 
+    private static final String TAG = "";
     BottomNavigationView bottomNavigationView;
     FloatingActionButton floatingActionButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +45,30 @@ public class Reminder extends AppCompatActivity {
         floatingActionButton = findViewById(R.id.Fab);
         bottomNavigationView.setBackground(null);
         bottomNavigationView.getMenu().getItem(2).setEnabled(false);
+
+
+        // Initialize Firebase Database
+        FirebaseDatabase db = FirebaseDatabase.getInstance("https://agrismartwatering-default-rtdb.firebaseio.com/");
+
+        //Refer database
+        DatabaseReference mDatabaseref = db.getReference();
+
+
+        String name = "dana";
+        // code under construction
+       /** Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat mdFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss:a");
+        String strDate =  mdFormat.format(calendar.getTime());
+        Toast.makeText(Reminder.this, ""+ (strDate), Toast.LENGTH_LONG).show();**/
+        mDatabaseref.child("Reminder"+ (name)).child("Time").setValue(ServerValue.TIMESTAMP);
+
+
+
+
+
+
+
+
 
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,6 +119,11 @@ public class Reminder extends AppCompatActivity {
                 return false;
             }
         });
+
+
+
+
+
 
 
     }
